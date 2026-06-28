3bfeedb818eeb0d3ad1541a0accf719c605310ce
Abdelrahman <abdelrahman.mohamed2178@gmail.com>
2026-06-28 09:21:00 +0300

    create users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
+            $table->string('number', 20)->unique();
+            $table->date('birth_day');
+            $table->enum('gender', ['Male', 'Female']);
+            $table->string('nationality');
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
